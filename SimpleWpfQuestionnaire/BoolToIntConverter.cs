848958fe7c6487591619cc012034bf0c2e41c487
Matthias Glöckner <mail@matthias-gloeckner.de>
2020-05-05 22:58:37 +0200

    Initial commit of May 05, 2020.

---
+++ SimpleWpfQuestionnaire/BoolToIntConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+
+namespace SimpleWpfQuestionnaire
+{
+    public class BoolToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (int)value == int.Parse(parameter.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.Equals(true) ? parameter : Binding.DoNothing;
+        }
+    }
+}
---
+++ SimpleWpfQuestionnaire/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+
+namespace SimpleWpfQuestionnaire
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public int[] Answers { get; set; }
+        public string PercentageOfTingles { get; set; }
+
+        public ICommand ResetCommand { get; }
+        public ICommand ResultCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            ResetCommand = new RelayCommand(Reset);
+            ResultCommand = new RelayCommand(ShowResult, CanShowResult);
+
+            Reset();
+        }
+
+        private void Reset()
+        {
+            Answers = new int[5] { -1, -1, -1, -1, -1 };
+            RaisePropertyChanged("Answers");
+
+            PercentageOfTingles = string.Empty;
+            RaisePropertyChanged("PercentageOfTingles");
+        }
+
+        private void ShowResult()
+        {
+            PercentageOfTingles = string.Format("You get {0:d}% of tingles.", SumOfAnswers() * 10);
+            RaisePropertyChanged("PercentageOfTingles");
+        }
+
+        private int SumOfAnswers()
+        {
+            int sum = 0;
+
+            foreach (int answer in Answers)
+                sum += answer;
+
+            return sum;
+        }
+
+        private bool CanShowResult()
+        {
+            bool questionnaireCompleted = true;
+
+            foreach (int answer in Answers)
+                questionnaireCompleted = questionnaireCompleted && (answer >= 0) && (answer <= 2);
+
+            return questionnaireCompleted;
+        }
+    }
+}
